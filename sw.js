e95022d443eb7e120d0e3983c3dfac4521872c45
dev-khaiduy <103346903+dev-khaiduy@users.noreply.github.com>
2023-06-16 10:50:33 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyDU5MIE4-FCKOiQpftQmL7fEhyBx3lPoxE',
+    messagingSenderId: '121599275924',
+    appId: '1:121599275924:web:576b53586ed753f746d581',
+    projectId: 'aka-website-1f766',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BSUH1PDDQFBSPLEV97HGG26AH0G',
+    siteid: '83d11265053e69f9f253c4db87610842'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
